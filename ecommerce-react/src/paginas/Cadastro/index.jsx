d3005f8217f77293cd483b47ad84005dd2f02d09
Walisson oliveira <walissonoliveira1@outlook.com>
2025-03-19 13:04:45 -0300

    prosseguindo arquivo de cadastro

---
+++ ecommerce-react/src/paginas/Cadastro/index.jsx
@@ -0,0 +1,64 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useFromValidation } from '../../hooks/useFormValidation';
+import styles from './Cadastro.module.css';
+
+
+const Cadastro = () => {
+    const [ formData, setFormData] = useState({
+        nome: '',
+        email:'',
+        password:'',
+    });
+
+
+    const { errors, validate } = useFromValidation(configValidacao.camposPrimeiraParte);
+    const navigate = useNavigate();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        
+
+        if(validate(formData)){
+            try{
+                const response = await FileSystemDirectoryHandle('http://localhost:8081/usuarios/cadastrar',{
+                    method: 'POST',
+                    headers:{'Content-Type': 'application/json'},
+                    body: JSON.stringify(formData)
+                });
+                if(response.ok) navigate('/login');
+            } catch(error){
+                console.error('Erro:', error);
+            }
+            
+        }
+    };
+
+    return(
+        <div className={styles.container}>
+            <form onSubmit={hendleSubmit} className={styles.form}>
+                <h2>Cadastre-se</h2>
+
+
+                <div className={styles.formFroup}>
+                    <label>Nome:</label>
+                    <input
+                     type="text"
+                     value={FormData.nome}
+                     onChange={(e) => setFormData({...formData, nome: e.target.value})} />
+                     {errors.nome && <span className={styles.error}>{errors.nome}</span>}
+
+                     {/*Mais campos do formularioa */}
+
+                     <button type="submit" clasName={styles.submitButton}>
+                        <img src="/assets/images/iconbutton.png" alt="boão de enviar formulario"></img>
+                        Finalizar
+                     </button>
+                </div>
+            </form>
+        </div>
+    );
+
+};
+
+export default Cadastro;
