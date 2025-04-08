1481b20162c16d0297c3dd3648b889adb5cabea0
Walisson oliveira <walissonoliveira1@outlook.com>
2025-04-08 11:40:54 -0300

    Validações de login

--- front-end-tradicional/js/validaLogin.js
+++ front-end-tradicional/js/validaLogin.js
@@ -27,3 +27,79 @@ const limparErrosLogin = () =>{
     
 }
 
+const validarLogin = () => {
+    limparErrosLogin();
+    let isValid = true;
+
+    const emailInput = document.getElementById('email');
+    if(!configValidacaoLogin.email.pegex.test(emailInput.value.trim())){
+        mostrarErroLogin(emailInput, configValidacaoLogin.email.mensagem);
+        isValid = false;
+    }
+    const passwordInput = document.getElementById('password');
+if(passwordInput.value.length < configValidacaoLogin.senha.min){
+    mostrarErroLogin(passwordInput,configValidacaoLogin.senha.mensagem);
+    isValid = false;
+
+}
+return isValid;
+};
+
+const realizarLogin = async(event) => {
+    event.preventDefault();
+
+
+    if(!validarLogin()) return;
+
+    const loginData = {
+        username: document.getElementById('email').value,
+        password: document.getElementById('password').value
+    };
+    try{
+        const response = await fetch ('http://localhost:8081/auth/Login',{
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json',
+
+            },
+            body: JSON.stringify(loginData)
+        });
+        if(!response.ok){
+            throw new Error('Credenciais inválidas');
+        }
+
+        const data = await response.json();
+
+        // Armazena o token JWT 
+        localStorage.setItem('jwtToken', data.token);
+        localStorege.setItem('userType', data.tipoUsuario);
+
+        
+       
+        switch(data.tipoUsuario){
+            case 'CLIENTE':
+                window.location.href = 'ClienteAuth.html';
+                break;
+                case 'FORNECEDOR':
+                    window.location.href = '#fornecedor';
+                    break;
+
+                case 'ADMINISTRADOR':
+                    window.location.href ='#admin';
+                    break;
+                default:
+                    window.location.href ='login.html';
+        }
+        
+
+    } catch(error){
+        console.error('erro no login:', error);
+        alert(error.message || 'Erro ao realizar login');
+
+    }
+};
+
+document.getElementById(loginForm).addEventListener('submit', realizarLogin);
+
+
+
