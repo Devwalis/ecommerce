453fcd68723f04a2566b7b3cb5dd663258e3c25b
Walisson oliveira <walissonoliveira1@outlook.com>
2025-04-17 23:40:48 -0300

    Stylização de navegação

---
+++ front-end-tradicional/js/admPerfil.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', function(){
+    const dropdown = document.querySelector('.dropdown-perfil');
+    const toggleBtn = document.querySelector('.perfil-toggle');
+
+
+    toggleBtn.addEventListener('click', function(e){
+        e.stopPropagation();
+        dropdown.classList.toggle('active');
+    });
+    document.addEventListener('click', function(e){
+        if(!dropdown.contains(e.target)){
+            dropdown.classList.remove('active')
+        }
+    });
+
+    window.addEventListener('scroll', function(){
+        dropdown.classList.remove('active')
+    })
+})
